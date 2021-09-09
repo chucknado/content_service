bd07f201dddbd5037ef8fc087ecf0519a68203d0
chucknado <cnadeau@zendesk.com>
2021-09-09 15:15:11 -0700

    Initial files

--- js/content_service.js
+++ js/content_service.js
@@ -1,1 +1,34 @@
-// insert content in page
+/* Inserts service content in the page */
+
+
+function insert_service_content() {
+  const catalog_url = 'https://cdn.jsdelivr.net/gh/chucknado/content_service@main/content/catalog.json';
+  const catalog = JSON.parse(get_file(catalog_url));
+  console.log(catalog);
+  // read all the elements with class "content_ph"
+  // read the "data-conref" attribute of each element to get the id of the content
+  // use the id to look up the content url in the catalog
+  // get the content
+  // insert the content at the placeholder
+}
+
+
+function get_file(url) {
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = handleResponse;
+  request.open('GET', url);
+  request.send();
+
+  function handleResponse() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        return request.response;
+      } else {
+        console.log('There was a problem with the request - ' + request.status);
+      }
+    }
+  }
+}
+
+
+window.addEventListener('load', insert_service_content, false);
