5445e40fe0e30905bdb0724f3773b7d0723fedcc
chucknado <cnadeau@zendesk.com>
2021-09-13 10:46:16 -0700

    Reorganize files and add example content

--- js/content_service.js
+++ js/content_service.js
@@ -1,13 +1,11 @@
 function insert_content() {
-  const root = 'https://cdn.jsdelivr.net/gh/chucknado/content_service@latest/content/';
-
+  const content_root = 'https://cdn.jsdelivr.net/gh/chucknado/content_service@latest/content/';
   const placeholders = document.getElementsByClassName('content_ph');
   for (var i = 0; i < placeholders.length; i++) {
     let ph = placeholders[i];
     let conref = ph.getAttribute('data-conref');
     let locale = ph.getAttribute('data-locale');
-    let content_url = root + conref + '.html';
-    // let content_url = root + locale + '/' + conref + '.html';  // with locale
+    let content_url = content_root + locale + '/' + conref + '.html';
 
     let promise = new Promise(function(resolve, reject) {
       function reqListener() {
@@ -24,12 +22,12 @@ function insert_content() {
     });
 
     promise.then(function(content) {
-      // insert content in div
       ph.innerHTML = content;
     }).catch(function(error) {
       ph.innerHTML = 'This content is currently not available.';
       console.log(error);
     });
+
   }
 }
 
